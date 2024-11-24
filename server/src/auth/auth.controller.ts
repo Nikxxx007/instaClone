dfcebd359edd49ff902ad5fa71949c1da29b66a1
Nikita Nefed <nick.nefed@gmail.com>
2024-11-24 23:24:25 +0300

    added packages and controller for auth

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(@Body() userDto: any) {
+    return this.authService.register(userDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('profile')
+  getProfile(@Body() req) {
+    return req.user;
+  }
+}
