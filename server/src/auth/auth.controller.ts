f4823691d1048bae09f84f860d5c7ea222c1b4f8
Nikita Nefed <nick.nefed@gmail.com>
2025-01-20 23:47:33 +0300

    auth and users modules are in progress

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,24 +1,34 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+// import { AuthService } from './auth.service';
 
+import { AuthGuard } from '@nestjs/passport';
+// import { RegisterRequestDto } from './dtos/register-request.dto';
+// import { LoginResponseDTO } from './dtos/login-response.dto';
+// import { RegisterResponseDTO } from './dtos/register-response.dto';
+// import { Public } from './decorators/public.decorator';
+
+// @Public()
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
+  @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Body() req) {
+  async login(@Request() req): Promise<LoginResponseDTO | BadRequestException> {
     return this.authService.login(req.user);
   }
 
   @Post('register')
-  async register(@Body() userDto: any) {
-    return this.authService.register(userDto);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Post('profile')
-  getProfile(@Body() req) {
-    return req.user;
+  async register(
+    @Body() registerBody: RegisterRequestDto,
+  ): Promise<RegisterResponseDTO | BadRequestException> {
+    return await this.authService.register(registerBody);
   }
 }
