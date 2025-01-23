1ca5817ca82d9ed0ff80e0f541f4ec733df6648d
Nikita Nefed <nick.nefed@gmail.com>
2025-01-23 22:27:05 +0300

    setting up database

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseProviders } from './database.providers';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // Тип базы данных
-      host: 'localhost', // Хост базы данных
-      port: 5432, // Порт подключения
-      username: 'postgres', // Имя пользователя
-      password: 'password', // Пароль
-      database: 'my_database', // Название базы данных
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Пути к сущностям
-      synchronize: true, // Синхронизация структуры базы данных (не рекомендуется в продакшне)
-    }),
-  ],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
 })
 export class DatabaseModule {}
