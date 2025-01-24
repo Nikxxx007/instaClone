bb08eeb6f3cf12e110ea3afa410aa6e044414404
Nikita Nefed <nick.nefed@gmail.com>
2025-01-24 22:55:19 +0300

    added validation for user

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3, { message: 'Имя пользователя должно быть не менее 3 символов' })
+  username: string;
+
+  @IsEmail({}, { message: 'Некорректный email' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Пароль должен быть не менее 6 символов' })
+  password: string;
+}
