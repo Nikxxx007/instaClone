90fe6a0df9c28a7ae23a029f9b19833cedd15c90
Nikita Nefed <nick.nefed@gmail.com>
2025-01-25 20:54:15 +0300

    added config via .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Инициализация ConfigModule
+    ConfigModule.forRoot({
+      isGlobal: true, // Делаем модуль глобальным (не нужно импортировать в других модулях)
+    }),
+
+    // Настройка TypeOrmModule
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true, // Автоматически загружает сущности
+        synchronize: true, // Автоматически синхронизирует структуру БД (в продакшене отключить!)
+      }),
+    }),
+  ],
 })
+
+// @Module({
+//   imports: [AuthModule, UsersModule, DatabaseModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
 export class AppModule {}
