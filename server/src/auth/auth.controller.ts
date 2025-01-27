3275e905714a96e9127ccf1dcd4f24a926546775
Nikita Nefed <nick.nefed@gmail.com>
2025-01-27 22:13:17 +0300

    removed db directory

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -15,20 +15,20 @@ import { AuthGuard } from '@nestjs/passport';
 // import { Public } from './decorators/public.decorator';
 
 // @Public()
-@Controller('auth')
-export class AuthController {
-  constructor(private authService: AuthService) {}
-
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req): Promise<LoginResponseDTO | BadRequestException> {
-    return this.authService.login(req.user);
-  }
-
-  @Post('register')
-  async register(
-    @Body() registerBody: RegisterRequestDto,
-  ): Promise<RegisterResponseDTO | BadRequestException> {
-    return await this.authService.register(registerBody);
-  }
-}
+// @Controller('auth')
+// export class AuthController {
+//   constructor(private authService: AuthService) {}
+//
+//   @UseGuards(AuthGuard('local'))
+//   @Post('login')
+//   async login(@Request() req): Promise<LoginResponseDTO | BadRequestException> {
+//     return this.authService.login(req.user);
+//   }
+//
+//   @Post('register')
+//   async register(
+//     @Body() registerBody: RegisterRequestDto,
+//   ): Promise<RegisterResponseDTO | BadRequestException> {
+//     return await this.authService.register(registerBody);
+//   }
+// }
