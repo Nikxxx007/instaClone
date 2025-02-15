869975c69ee10a512ae76698344a423db555ec7b
Nikita Nefed <nick.nefed@gmail.com>
2025-02-15 20:53:13 +0300

    added types for auth service

--- client/services/auth.service.ts
+++ client/services/auth.service.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import {LoginUserDto, RegisterUserDto} from "@/services/models";
 
 const API_URL = 'http://localhost:3000';
 
-export const register = async (userData: any) => {
+export const register = async (userData: RegisterUserDto) => {
   const response = await axios.post(`${API_URL}/auth/register`, userData);
   if (response.data.access_token) {
     localStorage.setItem('user', JSON.stringify(response.data));
@@ -10,7 +11,7 @@ export const register = async (userData: any) => {
   return response.data;
 };
 
-export const login = async (userData: any) => {
+export const login = async (userData: LoginUserDto) => {
   const response = await axios.post(`${API_URL}/auth/login`, userData);
   if (response.data.access_token) {
     localStorage.setItem('user', JSON.stringify(response.data));
---
+++ client/services/models.ts
@@ -0,0 +1,10 @@
+export interface RegisterUserDto {
+  email: string;
+  password: string;
+  username: string;
+}
+
+export interface LoginUserDto {
+  email: string;
+  password: string;
+}
